31cc28964691449bd9fb7cf9a602e13e5d7886d8
CathyyyZ <xiz236@ucsd.edu>
2025-11-25 21:31:05 -0800

    加照片

--- public/js/galleryRender.js
+++ public/js/galleryRender.js
@@ -16,7 +16,8 @@ function renderGallery() {
   //  path names will be appended automatically
   //  images will appear in this order
   var strs = ["Bonefire2024Fall.JPG", "毕业酒会2024.JPG","LB2024-2025.JPG", "DSC01734.JPG", "DSC01752.JPG",
-    "B0001140.jpg", "463A8617.JPG", "B0001132.jpg", "Copy of DSC_0712.JPG", 
+    "B0001140.jpg", "463A8617.JPG", "B0001132.jpg", "Copy of DSC_0712.JPG", "WechatIMG99.jpg", "WechatIMG100.jpg", "WechatIMG102.jpg",
+    
   ];
 
   var node = document.getElementById("gallery-content");
